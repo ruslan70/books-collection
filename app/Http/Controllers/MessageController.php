88edb203ec08304d4bf4da63e067b3a9ba456ea2
ruslan70 <ruslan.khatuev70@gmail.com>
2022-06-13 11:07:20 +0200

    add new use-Statement, addapt function showAll()

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Message;
 
 class MessageController extends Controller
 {
-    //
     public function showAll() {
-        return view('messages');
+ 
+        // gets all the entries from table messages
+        // and gets an array of objects as a return value.
+        // we store this return value in the variable $messages
+        $messages = Message::all()->sortByDesc('created_at');
+  
+        // This line would output the messages in the UI/Browser
+        // and stop the script execution.
+        // good for debugging ;o)
+        //dd($messages);
+  
+        // This function returns a view.
+        // here the blade template engine works its magic again
+        // through which we can pass the $messages array to the view.
+        // we can pass it as an optional second parameter (
+        // associative array)
+        return view('messages', ['messages' => $messages]);
+  
     }
+ 
  
 }
